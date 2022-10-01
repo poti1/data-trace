922725517bd5a114863327d805e2d120626e7949
Timofey Potapov <timofey.potapov@otrs.com>
2022-10-01 17:28:55 +0200

    0.07    2022-10-01 - Tidy.

--- lib/Data/Trace.pm
+++ lib/Data/Trace.pm
@@ -17,11 +17,11 @@ Data::Trace - Trace when a data structure gets updated.
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
