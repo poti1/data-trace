528983a632563a88ecf835e1dc84ca5a2691ff2b
Timofey Potapov <timofey.potapov@otrs.com>
2022-10-11 10:24:50 +0200

    0.08 - Fixed new git flow.

--- lib/Data/Trace.pm
+++ lib/Data/Trace.pm
@@ -17,11 +17,11 @@ Data::Trace - Trace when a data structure gets updated.
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
